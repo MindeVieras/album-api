1ecc18ead0439f33e46eb78a065e822d2db7efbc
MindeVieras <wieraz21@gmail.com>
2018-01-18 21:19:58 +0000

    main index es6

--- src/index.js
+++ src/index.js
@@ -1,70 +1,26 @@
 
-// import http from 'http';
-// import express from 'express';
-// import morgan from 'morgan';
-// import bodyParser from 'body-parser';
-// import config from './app/config.json';
+import http from 'http';
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import config from './config.json';
 
-// let app = express();
-// app.server = http.createServer(app);
+let app = express();
+app.server = http.createServer(app);
 
-// // logger
-// app.use(morgan('dev'));
-
-// app.use(bodyParser.json({
-//   limit : config.bodyLimit
-// }));
-
-// app.get('/', function (req, res) {
-//   res.send('hello world')
-// })
-
-// app.server.listen(process.env.PORT || config.port, () => {
-//   console.log(`Started on port ${app.server.address().port}`);
-// });
-
-// export default app;
-
-
-const port = process.env.PORT || 3000;
-const http = require('http');
-const path = require('path');
-// const fallback = require('express-history-api-fallback');
-const express = require('express');
-// const cors = require('cors');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-const app = express();
-
+// logger
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({
-  extended: true,
-  limit: '50mb'
+
+app.use(bodyParser.json({
+  limit : config.bodyLimit
 }));
-app.use(bodyParser.json());
 
-// const corsOptions = {
-//   origin: '*',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-// }
-// app.use(cors(corsOptions));
-// app.use(fallback(__dirname + './index.html'));
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send('hello world')
-
-  // res.sendFile(path.join(__dirname, './index.html'));
 })
 
-// require('./routes/authenticate')(app);
-// require('./routes/users')(app);
-// require('./routes/albums')(app);
-// require('./routes/media')(app);
-// require('./routes/upload')(app);
-// require('./routes/trash')(app);
-// require('./routes/front')(app);
-
-// Start HTTP server
-http.createServer(app).listen(port, function(){
-  console.log('Server listening on port ' + port);
+app.server.listen(process.env.PORT || config.port, () => {
+  console.log(`Started on port ${app.server.address().port}`);
 });
+
+export default app;
