cb77d6a71c1516bb78769c099214d55ab8021638
MindeVieras <wieraz21@gmail.com>
2018-02-09 01:10:50 +0000

    fromt circle menu position save

--- src/models/utils.js
+++ src/models/utils.js
@@ -70,3 +70,51 @@ export function saveAdminSetting(req, res) {
       res.json({ack:'err', msg})
     })
 }
+
+// Gets Front settings
+export function getFrontSettings(req, res) {
+  if (typeof req.params.id != 'undefined' && !isNaN(req.params.id) && req.params.id > 0 && req.params.id.length) {
+    const { id } = req.params
+    let settings = new Object()
+    conn.query(`SELECT * FROM users_settings WHERE user_id = ? AND type = 'front'`, id)
+      .then( rows => {
+        // let settingsObj = new Object()
+        rows.map((s) => {
+          settings[s.name] = s.value
+        })
+
+        // Return settings
+        res.json({ack:'ok', msg: 'Front settings', data: settings});
+      })
+      .catch( err => {
+        let msg = err.sqlMessage ? err.sqlMessage : err
+        res.json({ack:'err', msg})
+      })
+  
+  } else {
+    res.json({ack:'err', msg: 'bad parameter'})
+  }
+}
+
+// Saves front setting
+export function saveFrontSetting(req, res) {
+  const { name, value, uid } = req.body
+
+  let data = [ value, uid, name ]
+
+  conn.query(`UPDATE users_settings
+                SET value = ?
+              WHERE user_id = ? AND name = ? AND type = 'front'`, data)
+    .then( row => {      
+      if (row.affectedRows === 1) {
+        res.json({ack:'ok', msg: 'Setting saved'});
+      }
+      else {
+        throw 'Setting not saved'
+      }
+    })
+    .catch( err => {
+      let msg = err.sqlMessage ? err.sqlMessage : err
+      res.json({ack:'err', msg})
+    })
+}
--- src/routes/utils.js
+++ src/routes/utils.js
@@ -1,11 +1,21 @@
 
-import { getAppSettings, getAdminSettings, saveAdminSetting } from '../models/utils'
+import {
+  getAppSettings,
+  getAdminSettings,
+  saveAdminSetting,
+  getFrontSettings,
+  saveFrontSetting
+} from '../models/utils'
 const Auth = require('../helpers/authenticate');
 
 module.exports = function(app) {
 
   app.get('/api/utils/get-app-settings', getAppSettings)
+
   app.get('/api/utils/get-admin-settings/:id', Auth.isAdmin, getAdminSettings)
   app.post('/api/utils/save-admin-setting', Auth.isAdmin, saveAdminSetting)
+
+  app.get('/api/utils/get-front-settings/:id', Auth.isAuthed, getFrontSettings)
+  app.post('/api/utils/save-front-setting', Auth.isAdmin, saveFrontSetting)
 
 }
