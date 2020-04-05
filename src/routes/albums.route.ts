08c784e7958aeef84fe55f04769f562a662158ff
MindeVieras <mindevieras@gmail.com>
2020-04-05 01:18:45 +0100

    change album status from blocked to private

--- src/routes/albums.route.ts
+++ src/routes/albums.route.ts
@@ -4,19 +4,6 @@ import { AlbumController } from '../controllers'
 import { paramValidation, isAuthed } from '../config'
 import { validator } from '../middleware'
 import { UserRoles } from '../enums'
-// import { isAuthed } from '../helpers/authenticate'
-// import {
-//   getList,
-//   getListDates,
-//   getOne,
-//   create,
-//   rename,
-//   changeDate,
-//   setLocation,
-//   updateLocation,
-//   removeLocation,
-//   moveToTrash,
-// } from '../controllers/albums.model'
 
 /**
  * Create Albums router.
@@ -45,16 +32,5 @@ router
     validator.body(paramValidation.albumPatchBody),
     Album.updateOne,
   )
-
-// router.post('/list', isAuthed, getList)
-// router.get('/list-dates', isAuthed, getListDates)
-// router.get('/one/:id', isAuthed, getOne)
-// router.post('/create', isAuthed, create)
-// router.post('/rename', isAuthed, rename)
-// router.post('/change-date', isAuthed, changeDate)
-// router.post('/set-location', isAuthed, setLocation)
-// router.post('/update-location', isAuthed, updateLocation)
-// router.get('/remove-location/:id', isAuthed, removeLocation)
-// router.delete('/move-to-trash/:id', isAuthed, moveToTrash)
 
 export default router
