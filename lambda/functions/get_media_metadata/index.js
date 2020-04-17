ef30396c17d01e4c07b64f79d9434cd1d336bb06
MindeVieras <mindevieras@gmail.com>
2020-04-17 23:19:04 +0100

    get_video_metadata lambda function is removed, get_media_metadata instead

--- lambda/functions/get_media_metadata/index.js
+++ lambda/functions/get_media_metadata/index.js
@@ -1,5 +1,7 @@
 const AWS = require('aws-sdk')
 const ExifParser = require('exif-parser')
+const ffprobe = require('ffprobe')
+const moment = require('moment')
 
 const s3 = new AWS.S3()
 
@@ -19,12 +21,20 @@ exports.handle = async function(input) {
   // Input keys.
   const { bucket, key } = input
 
+  if (!bucket || !key) {
+    return {
+      success: false,
+      error: "Keys 'bucket' and 'key' are required to execute this function",
+    }
+  }
+
   try {
     const s3Params = {
       Bucket: bucket,
       Key: key,
     }
 
+    // Get content type.
     const { ContentType } = await s3.headObject(s3Params).promise()
 
     if (ContentType.includes('image') || ContentType.includes('video')) {
@@ -33,7 +43,7 @@ exports.handle = async function(input) {
 
       // Handle images.
       if (ContentType.includes('image')) {
-        // Parse exif data from images.
+        // Parse exif data from the image.
         const parser = ExifParser.create(s3File.Body)
         const { tags, imageSize } = parser.parse()
         return {
@@ -44,12 +54,23 @@ exports.handle = async function(input) {
             ...prepareExifTags(tags),
           },
         }
-      }
+      } else {
+        // Handle videos.
 
-      // Handle videos.
-      return {
-        success: true,
-        data: 'Video meta goes here...',
+        // Get signed url to use as external url.
+        // Set expire to minimum.
+        const url = s3.getSignedUrl('getObject', {
+          Bucket: bucket,
+          Key: key,
+          Expires: 60,
+        })
+        // ffprobe is added to '/opt/bin/ffprobe' by lambda layer.
+        const ffprobeData = await ffprobe(url, { path: '/opt/bin/ffprobe' })
+
+        return {
+          success: true,
+          data: prepareFfprobeTags(ffprobeData.streams),
+        }
       }
     }
 
@@ -61,10 +82,11 @@ exports.handle = async function(input) {
 }
 
 /**
- * Helper function to format parsed exif data.
+ * Helper function to format parsed exif data from image.
  *
  * @param {object} parsedExifData
  *   Exif data parsed from the image.
+ *
  * @return {object}
  *   Formatted exif data.
  */
@@ -74,7 +96,7 @@ function prepareExifTags(tags) {
   if (tags) {
     Object.keys(tags).forEach((key) => {
       const tag = tags[key]
-      if (key === 'DateTimeOriginal') meta.timestamp = tag
+      if (key === 'DateTimeOriginal') meta.datetime = moment.unix(tag)
       if (key === 'Flash') meta.flash = tag
       if (key === 'ISO') meta.iso = tag
 
@@ -93,6 +115,36 @@ function prepareExifTags(tags) {
       }
       if (tags.GPSAltitudeRef || tags.GPSAltitudeRef === 0) {
         meta.location = { ...meta.location, altRef: tags.GPSAltitudeRef }
+      }
+    }
+  }
+
+  return meta
+}
+
+/**
+ * Helper function to format metadata extracted from video.
+ *
+ * @param {object} streams
+ *   List of ffprobe streams.
+ *
+ * @returns {object}
+ *   Metadata for the video.
+ */
+function prepareFfprobeTags(streams) {
+  let meta = {}
+  if (streams) {
+    // make meta object
+    for (let i = 0; i < streams.length; i++) {
+      const stream = streams[i]
+      if (stream.codec_type === 'video') {
+        meta.width = stream.width
+        meta.height = stream.height
+        meta.duration = parseFloat(stream.duration)
+        meta.frameRate = eval(stream.r_frame_rate)
+        meta.codec = stream.codec_name
+        if (stream.tags && 'creation_time' in stream.tags)
+          meta.datetime = moment(stream.tags.creation_time)
       }
     }
   }
